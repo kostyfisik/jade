94de95f1a0d8631d46ecfd1228d00ad34ea6b517
Konstantin Ladutenko <kostyfisik@gmail.com>
2015-02-09 21:38:26 +0300

    two layer optimize share

--- go.sh
+++ go.sh
@@ -84,8 +84,9 @@ fi
 #jade_bin="run-size-sweep"
 #jade_bin="run-optimize-cloak"
 #jade_bin="run-coating-w-sweep"
-jade_bin="run-coating-w-sweep-2layers"
+#jade_bin="run-coating-w-sweep-2layers"
 #jade_bin="run-optimize-meander-cloak"
+jade_bin="run-optimize-meander-cloak-2layer"
 #jade_bin="run-quasi-pec-spectra"
 if [[ $mode = $mode_test ]]; then
     echo Run JADE++ test!
